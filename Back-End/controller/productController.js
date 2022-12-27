3a64ea291a0fb05cf964f566abf15fc50910dc22
SomonSikder <somonsikder777@gmail.com>
2022-12-27 15:32:20 +0600

    User modle and controller

--- Back-End/controller/productController.js
+++ Back-End/controller/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../modles/Product");
+const Product = require("../modles/ProductModle");
 const ErrorHandler = require("../middleware/error");
 const Features = require("../utils/features");
 
